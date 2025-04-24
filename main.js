9c94716f5f4d26706e1f2cf332864f24232197ac
akunml6925 <akunml6925@gmail.com>
2025-04-24 16:59:58 +0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 let dataTugas = [];
 let nomor = 1;
 
